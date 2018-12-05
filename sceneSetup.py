64e6dc2e90f530b7ac1ae610ba6a1e6e9f94c03d
Gabriel Berthold <gabbe631@student.liu.se>
2018-12-05 17:27:42 +0100

    particles

--- sceneSetup.py
+++ sceneSetup.py
@@ -1,5 +1,5 @@
 import maya.cmds as cmds
-
+import random
 #Create new scene
 cmds.file(f=True, new=True)
 
@@ -15,3 +15,22 @@ wall = cmds.polyCube(w=0.01, d=10, h=2, sx=10, sy=10, sz=10, name='wall3')
 cmds.move(-5, 1, 0, wall)
 wall = cmds.polyCube(w=0.01, d=10, h=2, sx=10, sy=10, sz=10, name='wall4')
 cmds.move(5, 1, 0, wall)
+
+
+width = 5
+depth = 5
+height = 2
+
+
+for w in range (-5, width):
+    for d in range (-5, depth):
+        for h in range (height):
+            particles = cmds.polySphere(r=0.3, name='par#')
+            cmds.move(w+ 0.5, h +0.5, d+ 0.5, particles)
+            
+            
+cmds.group('par*', name='AllParticles')
+cmds.gravity(m= 9.82, dx = 0, dy =-1 , dz = 0, vsh = 'none', name='grav')
+
+cmds.connectDynamic(f='grav', 'AllParticles')
+            
