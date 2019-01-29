779f4ee15a827b1515c7a104067e8fae8242f526
Jonas Kinnvall <jonaskinnvall@gmail.com>
2019-01-29 09:07:32 +0100

    skit

--- sceneSetup.py
+++ sceneSetup.py
@@ -1,13 +1,13 @@
 import maya.cmds as cmds
-import random
-#Create new scene
+# import random
+# Create new scene
 cmds.file(f=True, new=True)
 
-#Create ground polyCube
+# Create ground polyCube
 cmds.polyPlane(w=20, h=20, sx=10, sy=10, name='ground')
 
 
-#Create wall polyCubes
+# Create wall polyCubes
 wall = cmds.polyPlane(w=20, h=2, sx=10, sy=10, name='wall1')
 cmds.move(0, 1, -10, wall)
 cmds.rotate('90deg', 0, 0, r=True)
@@ -27,35 +27,27 @@ cmds.rotate(0, 0, '90deg', r=True)
 cmds.group('ground', 'wall1', 'wall2', 'wall3', 'wall4', name='box')
 cmds.select('box')
 cmds.rotate('15deg', 0, 0, r=True)
-cmds.rigidBody( passive=True, name='rigidBodyBox') 
+cmds.rigidBody(passive=True, name='rigidBodyBox')
 
-
-cmds.gravity(pos=[0,0,0], m= 9.82, dx = 0, dy =-1 , dz = 0, vsh = 'none', name='grav')
+cmds.gravity(pos=[0, 0, 0], m=9.82, dx=0, dy=-1, dz=0, vsh='none', name='grav')
 
 width = 5
 depth = 5
 height = 8
 
 
-for w in range (-5, -3):
-    for d in range (-5, depth):
-        for h in range (2, height):
+for w in range(-5, -3):
+    for d in range(-5, depth):
+        for h in range(2, height):
             particles = cmds.polySphere(r=0.3, name='par#')
-            cmds.move(w+ 0.5, h , d+ 0.5, particles)
+            cmds.move(w+0.5, h, d+0.5, particles)
             cmds.connectDynamic(particles, f='grav')
-            
-for w in range (3, 5):
-    for d in range (-5, depth):
-        for h in range (2, height):
-            particles = cmds.polySphere(r=0.3, name='par#')
-            cmds.move(w+ 0.5, h , d+ 0.5, particles)
-            cmds.connectDynamic(particles, f='grav')
-            
 
-            
-            
-            
+for w in range(3, 5):
+    for d in range(-5, depth):
+        for h in range(2, height):
+            particles = cmds.polySphere(r=0.3, name='par#')
+            cmds.move(w+0.5, h, d+0.5, particles)
+            cmds.connectDynamic(particles, f='grav')
+
 cmds.group('par*', name='AllParticles')
-
-
-            
