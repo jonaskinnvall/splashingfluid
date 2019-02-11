56dcfd217b4f52dba38d04e0fcc7858f0793c03a
Gabriel Berthold <gabbe631@student.liu.se>
2019-02-11 15:55:22 +0100

    photos of scenes

--- sceneSetup.py
+++ sceneSetup.py
@@ -2,13 +2,19 @@ import maya.cmds as cmds
 import maya.mel
 #Create new scene
 cmds.file(f=True, new=True)
+
+cmds.polyPlane(w=20, h=20)
+maya.mel.eval("makeCollideNCloth")
+
 #Create Box
-cmds.polyCube(w=1, h=5, d=1, ax=[0,1,0], name='cube')
-cmds.move(0,2.5,0, 'cube')
+#cmds.polyCube(w=3, h=1, d=3, ax=[0,1,0], name='cube')
+cmds.polyCube(w=0.5, h=3, d=0.5, ax=[0,1,0], name='cube')
+cmds.move(0,1.5,0, 'cube')
 cmds.select('cube.f[1]')
-cmds.polyExtrudeFacet(kft=True, ls=(0.99,0.99,0.99), ty=-4.99)
+cmds.polyExtrudeFacet(kft=True, ls=(0.99,0.99,0.99), ty=-0.99)
 cmds.select('cube')
 cmds.setAttr('lambert1.transparency', 0.9, 0.9, 0.9, type='double3')
+cmds.setAttr('lambert1.ambientColor', 0.7, 0.7, 0.7, type='double3')
 maya.mel.eval("makeCollideNCloth")
 cmds.select('cube')
 cmds.rigidBody(pas=True)
@@ -23,7 +29,7 @@ cmds.rigidBody(act=True)
 cmds.gravity(name='grav')
 cmds.connectDynamic('ball', f='grav')
 
-'''
+
 #Create bowl
 cmds.polySphere(r=3, ax=[0,1,0], cuv=2, name='bowl')
 cmds.move(0,3,0, 'bowl')
@@ -32,6 +38,7 @@ cmds.select('bowl.f[180:359]', 'bowl.f[380:399]')
 cmds.delete()
 cmds.select('bowl')
 cmds.setAttr('lambert1.transparency', 0.9, 0.9, 0.9, type='double3')
+cmds.setAttr('lambert1.ambientColor', 0.7, 0.7, 0.7, type='double3')
 maya.mel.eval("makeCollideNCloth")
 
 #Create Rod
@@ -41,12 +48,11 @@ cmds.select('rod')
 cmds.rotate(0,45,0)
 maya.mel.eval("makeCollideNCloth")
 
-'''
-#Create liquid1
-cmds.nParticle( ll=[-0.45,0.1,-0.45], ur=[0.45,5,0.45], grs=0.1)
 
-#cmds.nParticle( ll=[-1.4,35,-1.4], ur=[-1,65,-1], grs=0.1)
-#cmds.nParticle(ll=[-0.2, 5, -0.2], ur=[0.2, 60, 0.2], grs=0.2)
+#Create liquid1
+#cmds.nParticle( ll=[-0.45,0.1,-0.45], ur=[0.45,10,0.45], grs=0.2)
+#cmds.nParticle( ll=[-1.4,5,-1.4], ur=[-1,35,-1], grs=0.1)
+cmds.nParticle(ll=[-0.2, 5, -0.2], ur=[0.2, 30, 0.2], grs=0.1)
 cmds.select('nParticleShape1')
 cmds.setAttr('nParticleShape1.radius', 0.05)
 cmds.setAttr('nParticleShape1.enableSPH', 1)
